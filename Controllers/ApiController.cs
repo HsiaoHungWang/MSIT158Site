e4a4976cda22ae19effd003507ad653469eb4c2a
HsiaoHungWang <wang@ispan.com.tw>
2024-05-20 16:02:43 +0800

    MVC 回應文字、回應JSON

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT158Site.Models;
+
+namespace MSIT158Site.Controllers
+{
+    public class ApiController : Controller
+    {
+     private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return Content("<h2>世界, 您好!!</h2>","text/html", System.Text.Encoding.UTF8);
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+    }
+}
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,10 +5,18 @@ namespace MSIT158Site.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly MyDBContext _context;
+        public ProductsController(MyDBContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            //程式碼
            
-            return View();
+            return View();           
+            
         }
     }
 }
