f61e37718c399fe7d69990d54669e853ad9888a9
HsiaoHungWang <wang@ispan.com.tw>
2024-05-20 10:24:18 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSIT158Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
